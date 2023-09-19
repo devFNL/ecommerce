36cd568079c91d42f67c3b1d270f6609d23e5dcb
Federico Lindberg <fedelindberg@gmail.com>
2023-09-19 18:18:31 -0300

    init repo

---
+++ client/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme.js";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state";
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
+
+// npm run develop
+// para strapi
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);

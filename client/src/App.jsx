3fbe26fc137b2ea417017187b335680c18a98b3c
Federico Lindberg <fedelindberg@gmail.com>
2023-09-25 18:35:30 -0300

    path success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/item/:itemId" element={<ItemDetails />} />
             <Route path="checkout" element={<Checkout />} />
-            <Route path="/checkout/success" element={<Confirmation />} />
+            <Route path="checkout/success" element={<Confirmation />} />
           </Routes>
           <CartMenu />
           <Footer />

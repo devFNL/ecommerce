0450e8c11f095da99b520fdf0c2e2f8ad24476f8
Federico Lindberg <fedelindberg@gmail.com>
2023-09-26 13:09:34 -0300

    redirecc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
       <div className="app">
-        <BrowserRouter>
+        <BrowserRouter basename="/">
           <Navbar />
           <ScrollToTop />
           <Routes>
